27292c363386370327b33289c486a99ca635ebbb
kriskris-27 <krishnakumars0101@gmail.com>
2025-06-03 22:05:16 +0530

    reworked-1

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' 
+    baseURL: import.meta.env.PROD 
         ? 'https://thesis-server-wwmb.onrender.com/api'
         : 'http://localhost:5000/api',
     withCredentials: true
