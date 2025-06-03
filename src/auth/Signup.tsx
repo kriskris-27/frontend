e18cbd8a9b04c16765c0759617619f5962104b0d
kriskris-27 <krishnakumars0101@gmail.com>
2025-06-03 16:27:21 +0530

    api fixed

--- src/auth/Signup.tsx
+++ src/auth/Signup.tsx
@@ -68,7 +68,7 @@ export default function Signup() {
         </div>
 
         <a
-          href="http://localhost:5000/api/auth/google"
+          href="https://thesis-server-wwmb.onrender.com/api/auth/google"
           className="w-full flex justify-center items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
           <img src={gimg} alt="Google" className="h-5 w-5 mr-2" />
